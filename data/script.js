2ad2b4364dab2777a927c7aff8bd3c0e67fa3852
Manfe07 <manuel.fehren@gmail.com>
2025-08-15 18:16:14 +0200

    Connection Lost PopUp

--- data/script.js
+++ data/script.js
@@ -1,4 +1,3 @@
-
 var gateway = `ws://${window.location.hostname}/ws`;
 var websocket;
 // Init web socket when the page loads
@@ -31,14 +30,55 @@ function onClose(event) {
     setTimeout(initWebSocket, 2000);
 }
 
+// Heartbeat Überwachung
+let heartbeat = true;
+let heartbeatTimeout = null;
+
+
+
+function showConnectionLost() {
+    const popup = document.getElementById("connection-lost-popup");
+    if (popup) popup.style.display = "flex";
+}
+function hideConnectionLost() {
+    const popup = document.getElementById("connection-lost-popup");
+    if (popup) popup.style.display = "none";
+}
+
 // Function that receives the message from the ESP32 with the readings
 function onMessage(event) {
+    var data = event.data;
     console.log(event.data);
     var myObj = JSON.parse(event.data);
     var keys = Object.keys(myObj);
+    var mode = "Falsche Daten";
 
-    for (var i = 0; i < keys.length; i++){
-        var key = keys[i];
-        document.getElementById(key).innerHTML = myObj[key];
+    switch (myObj["mode"]){
+        case "0":
+            var mode = "Taste Drücken!";
+            break;
+        case "1":
+            var mode = "Zum Starten Dribble-Platte auslösen";
+            break;
+        case "2":
+            var mode = "Restzeit";
+            break;
+        default:
+            var mode = "Falsche Daten";
+            break;
     }
-}
+    document.getElementById("mode").innerHTML = mode;
+    document.getElementById("counter").innerHTML = myObj["counter"];
+    document.getElementById("countdown").innerHTML = myObj["countdown"];
+
+
+    hideConnectionLost()
+    if (heartbeatTimeout){
+        clearTimeout(heartbeatTimeout);
+    }
+    // Wenn nach 2 Sekunden kein neuer Heartbeat, Verbindung als verloren anzeigen
+    heartbeatTimeout = setTimeout(() => {
+        heartbeat = false;
+        showConnectionLost();
+    }, 2000);
+    }
