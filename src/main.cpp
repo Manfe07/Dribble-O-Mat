c0f245a15f3d926084c82341914dbe12bc381441
Manfe07 <manuel.fehren@gmail.com>
2025-08-16 14:43:14 +0200

    messi commit

--- src/main.cpp
+++ src/main.cpp
@@ -23,7 +23,7 @@
 const char* hostname = "Dribble-O-Mat";
 
 // Json Variable to Hold Sensor Readings
-DynamicJsonDocument readings(1024);
+StaticJsonDocument<1024> readings;
 
 Countdown countdown(60000); // Countdown timer for 1 second
 
@@ -38,13 +38,26 @@ static AsyncWebSocket ws("/ws");
 unsigned long lastTime = 0;
 unsigned long timerDelay = 30000;
 
-uint16_t counter = 0; // Counter for motion detection
+unsigned int counter = 0; // Counter for motion detection
 
+
+enum Mode {
+  normal,   // Normal mode, no motion detection
+  armed,    // Armed mode, motion detection is active
+  running   // Running mode, countdown is active
+};
+
+Mode gameMode = normal; // Initial mode is normal
+
+
+char buf[16];
 
 String getSensorReadings()
 {
-  readings["counter"] = String(counter);
+  readings.clear(); // Clear previous readings
+  readings["counter"] = String(counter); // Convert counter to string
   readings["countdown"] = String(int(countdown.timeLeft()/1000));
+  readings["mode"] = String(gameMode);
   String jsonString;
   serializeJson(readings, jsonString);
   return jsonString;
@@ -67,7 +80,7 @@ void initLittleFS()
 void initWiFi()
 {
   WiFi.mode(WIFI_AP_STA);  // Enable both AP and Station modes
-  WiFi.softAP(ap_ssid, ap_password);  // Start the access point
+  //WiFi.softAP(ap_ssid, ap_password);  // Start the access point
   WiFi.begin(sta_ssid, sta_password); // Connect to existing network
 }
 
@@ -174,50 +187,100 @@ void setup(void)
 
 ulong nextCall = 0;
 ulong lastMotion = 0;
-uint16_t motionTimeout = 200; // 200 ms timeout for motion detection
+uint16_t motionTimeout = 100; // 200 ms timeout for motion detection
 
 bool oldState = false; // Variable to store the previous state of the interrupt pin
+ulong lastButtonPress = 0; // Variable to store the last button press time
 
 void loop()
 {
   ulong currentMillis = millis();
 
-  if (digitalRead(buttonPin) == LOW)
+  // REGION: Button Handling
+  if (digitalRead(buttonPin) == LOW && oldState == true && lastButtonPress + 500 < currentMillis)
   {
-    if (!countdown.isRunning())
-    {
-      countdown.start(); // Start the countdown if it is not running
-      counter = 0;       // Reset the motion counter
-      Serial.println("Countdown started");
-    }
-  }
+    lastButtonPress = currentMillis; // Update the last button press time
+    Serial.println("Button pressed, changing mode");
 
+    // Change the game mode based on the current mode
+    switch (gameMode)
+    {
+      case normal:
+        gameMode = armed; // Change mode to armed
+        countdown.set(60000); // Set countdown to 60 seconds
+        Serial.println("Mode changed to armed");
+        break;
+      case armed:
+        gameMode = normal; // Change mode to normal
+        countdown.stop(); // Stop the countdown
+        Serial.println("Mode changed to normal");
+        break;
+      case running:
+        gameMode = normal; // Change mode to normal
+        countdown.stop(); // Stop the countdown
+        break;
+      default:
+        gameMode = normal; // Change mode to normal
+        break;
+    }
+    notifyClients(getSensorReadings());
+  }
+  oldState = digitalRead(buttonPin) == LOW; // Update the oldState variable
+  // ENDREGION: Button Handling
+
+
+  // REGION: MPU6050 Motion Detection
   if (mpu.getMotionInterruptStatus())
   {
     if (currentMillis - lastMotion > motionTimeout)
     {
       lastMotion = currentMillis;
       nextCall = currentMillis + 200; // Reset nextCall to avoid immediate re-triggering
+      
+      if (gameMode == armed){
+        gameMode = running; // Change mode to running
+        countdown.start(); // Start the countdown if it is not running
+        counter = 0;       // Reset the motion counter
+        Serial.println("Countdown started");
+      }
+
       if (countdown.isRunning())
       {
         counter++; // Increment the motion counter
       }
-      Serial.println("Motion detected! ");
-      Serial.println(currentMillis);
+      else if (gameMode == running)
+      {
+        gameMode = normal; // If countdown is not running, change mode to armed
+      }
+      else
+      {
+        /* code */
+      }
+      
       notifyClients(getSensorReadings());
-      Serial.println(millis());
     }
   }
+  // ENDREGION: MPU6050 Motion Detection
 
-  ws.cleanupClients();
 
+  // REGION: Countdown Handling
   if (nextCall < currentMillis)
   {
     if (countdown.isRunning())
       nextCall = currentMillis + 500; // If countdown is running, check every 200 ms
     else
       nextCall = currentMillis + 1000; // Otherwise, check every second
+
+    // If countdown is not running, change mode to armed
+    if (gameMode == running && !countdown.isRunning())
+      gameMode = normal;
+
+    // If countdown is running, notify clients with the current sensor readings
     notifyClients(getSensorReadings());
     digitalWrite(LED, !digitalRead(LED)); // Turn on the LED to indicate failure
   }
+  // ENDREGION: Countdown Handling
+
+  // Handle WebSocket events
+  ws.cleanupClients();
 }
